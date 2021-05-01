a0a001320cfdc2120b0c571b3881b74308f845f5
TwentyFive <TwentyFiveSoftware@gmail.com>
2021-05-01 17:12:34 +0200

    basic bot setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('test');
+import { Client } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new Client();
+
+client.on('ready', () => console.log('[STATUS] Bot is ready'));
+
+client.login(process.env['TOKEN']);
