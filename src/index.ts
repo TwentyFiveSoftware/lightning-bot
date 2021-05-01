18e3328aa1fa0aeeaa5d93cd8fc724008350d2db
TwentyFive <TwentyFiveSoftware@gmail.com>
2021-05-01 21:19:49 +0200

    add simple http server

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Client } from 'discord.js';
 import dotenv from 'dotenv';
 import { registerModules } from './registerModules';
 import fs from 'fs';
+import http from 'http';
 
 dotenv.config();
 if (!fs.existsSync('db')) fs.mkdirSync('db');
@@ -11,3 +12,8 @@ const client = new Client();
 registerModules(client);
 
 client.login(process.env['TOKEN']);
+
+// HTTP SERVER TO PREVENT HEROKU FROM CRASHING
+http.createServer((_req, res) => {
+    res.end(new Date().toString());
+}).listen(process.env['PORT'] || 3000);
